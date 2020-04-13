e4bc5271608efbe7e836e3f50ab772efc8cd78d9
Fffedr <feodor-arilou1@mail.ru>
2020-04-13 07:49:59 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int dp[50][50];
+    dp[1][1] = 1;
+    for(int i = 2; i <= n; i++)
+        for(int j = 2; j <= m; j++)
+            dp[i][j] = dp[i-1][j-2] + dp[i-2][j-1];
+    cout << dp[n][m];
+    return 0;
+}
